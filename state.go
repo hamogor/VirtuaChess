bcea292189810ef08c2399ce54a77921a46b96f9
Harry Morgan <orefdev@gmail.com>
2022-04-30 18:49:10 +0100

    added print move inputted

--- state.go
+++ state.go
@@ -16,13 +16,22 @@ func (g *Game) Start() {
 	// Player 2 chooses input
 	//
 	for {
-		fmt.Print("-> ")
+		fmt.Print("Player Input-> ")
 		text, _ := reader.ReadString('\n')
 		// convert CRLF to LF
 		text = strings.Replace(text, "\n", "", -1)
 
-		if strings.Compare("hi", text) == 0 {
-			fmt.Println("hello, Yourself")
+		if strings.Compare("p", text) == 0 {
+			fmt.Println("Inputted: Punch")
+		}
+		if strings.Compare("6p", text) == 0 {
+			fmt.Println("Inputted: Elbow")
+		}
+		if strings.Compare("2k", text) == 0 {
+			fmt.Println("Inputted: Low Kick")
+		}
+		if strings.Compare("6p+g", text) == 0 {
+			fmt.Println("Inputted: Throw")
 		}
 
 	}
